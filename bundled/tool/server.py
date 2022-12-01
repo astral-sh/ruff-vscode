3f9cc67cbde59eb5eedcb3616dc1d5dc30fb99a4
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-01 14:08:48 -0500

    Add name and version to LanguageServer (#38)

--- bundled/tool/server.py
+++ bundled/tool/server.py
@@ -42,7 +42,11 @@ WORKSPACE_SETTINGS = {}
 RUNNER = pathlib.Path(__file__).parent / "runner.py"
 
 MAX_WORKERS = 5
-LSP_SERVER = server.LanguageServer(max_workers=MAX_WORKERS)
+LSP_SERVER = server.LanguageServer(
+    name="Ruff",
+    version="2022.0.17",
+    max_workers=MAX_WORKERS,
+)
 
 
 # **********************************************************
