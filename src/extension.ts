20215be6328cd7ed6ec5763fdfa9007f54a2192a
konstin <konstin@mailbox.org>
2023-05-23 12:43:37 +0200

    Only one server restart at a time (#194)

--- src/extension.ts
+++ src/extension.ts
@@ -17,6 +17,8 @@ import { createOutputChannel, onDidChangeConfiguration, registerCommand } from '
 const issueTracker = 'https://github.com/charliermarsh/ruff/issues';
 
 let client: LanguageClient | undefined;
+let clientPromise: Promise<LanguageClient | undefined> | undefined = undefined;
+let isNewRestartQueued = false;
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     // This is required to get server name and module. This should be
@@ -35,7 +37,29 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     traceVerbose(`Configuration: ${JSON.stringify(serverInfo)}`);
 
     const runServer = async () => {
-        client = await restartServer(serverId, serverName, outputChannel, client);
+        if (clientPromise != null) {
+            traceLog(`Triggered ${serverName} restart while restart already in progress, queuing another restart`);
+            if (!isNewRestartQueued) {
+                // Schedule a new restart after the current one, but also make it so that there is one restart queue at a time,
+                // this restart will have the latest settings anyway.
+                isNewRestartQueued = true;
+                try {
+                    await clientPromise;
+                } catch {
+                    // We don't care whether this failed, we'll restart afterwards again anyway
+                }
+            } else {
+                // In this case, we're currently restarting and a new restart is also queued, so we just do nothing and
+                // let the other restart handle this
+                traceLog(
+                    `Triggered ${serverName} restart while restart already in progress and another already queued, doing nothing`,
+                );
+                return;
+            }
+        }
+        clientPromise = restartServer(serverId, serverName, outputChannel, client);
+        isNewRestartQueued = false;
+        await clientPromise;
     };
 
     context.subscriptions.push(
