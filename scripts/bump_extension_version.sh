215b1d510099492034935bf4796db2e8ecf4b618
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-06 21:45:57 -0400

    Update bump_extension_version.sh

--- scripts/bump_extension_version.sh
+++ scripts/bump_extension_version.sh
@@ -24,8 +24,7 @@ pip-compile --generate-hashes --resolver=backtracking --upgrade --extra dev -o .
 npm install --package-lock-only
 
 # Commit the change.
-git add .
-git commit -m "Bump extension version to $TO"
+git commit -am "Bump extension version to $TO"
 
 # Push.
 git push origin HEAD
