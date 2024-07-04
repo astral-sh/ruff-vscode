fbb04674f30f21a0a11f7aff1d40b0fe461150d8
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-07-04 09:19:30 +0530

    Keep Python script limited to `sysconfig` usage (#511)
    ## Summary
    
    This PR makes progress towards #476 and #479 by making the following
    changes:
    * Update the `ruff_server.py` script to limit itself to just getting the
    Ruff binary path using `sysconfig`
    * Use the `which` dependency and move the logic of locating the Ruff
    binary from `PATH` to TypeScript
    * Add log messages similar to `ruff-lsp`
    
    ## Test Plan
    
    ### 1
    
    ```json
    {
      "ruff.nativeServer": true
    }
    ```
    Or
    ```json
    {
      "ruff.nativeServer": true,
      "ruff.importStrategy": "fromEnvironment"
    }
    ```
    
    Logs:
    
    When the `ruff` binary is installed in the current virtual environment:
    ```
    2024-07-03 15:28:35.674 [info] Using the Ruff binary: /Users/dhruv/work/astral/ruff-vscode/.venv/bin/ruff
    2024-07-03 15:28:35.675 [info] Server run command: /Users/dhruv/work/astral/ruff-vscode/.venv/bin/ruff server --preview
    ```
    
    If there's no `ruff` binary in the current virtual environment:
    ```
    2024-07-03 15:25:19.605 [info] Using the Ruff binary: /Users/dhruv/.local/bin/ruff
    2024-07-03 15:25:19.606 [info] Server run command: /Users/dhruv/.local/bin/ruff server --preview
    ```
    
    If the `ruff` binary is only available via `PATH`:
    ```
    2024-07-03 15:32:12.172 [info] Using environment executable: /opt/homebrew/bin/ruff
    2024-07-03 15:32:12.173 [info] Server run command: /opt/homebrew/bin/ruff server --preview
    ```
    
    And, if there's no `ruff` binary anywhere on the system, use the bundled
    binary:
    ```
    2024-07-03 15:29:47.948 [info] Falling back to bundled executable: /Users/dhruv/work/astral/ruff-vscode/bundled/libs/bin/ruff
    2024-07-03 15:29:47.948 [info] Server run command: /Users/dhruv/work/astral/ruff-vscode/bundled/libs/bin/ruff server --preview
    ```
    
    ### 2
    
    ```json
    {
      "ruff.nativeServer": true,
      "ruff.importStrategy": "useBundled"
    }
    ```
    
    Logs:
    ```
    2024-07-03 15:26:28.767 [info] Using bundled executable: /Users/dhruv/work/astral/ruff-vscode/bundled/libs/bin/ruff
    2024-07-03 15:26:28.767 [info] Server run command: /Users/dhruv/work/astral/ruff-vscode/bundled/libs/bin/ruff server --preview
    ```
    
    ### 3
    
    ```json
    {
      "ruff.nativeServer": true,
      "ruff.path": ["/Users/dhruv/work/astral/ruff/target/debug/ruff"]
    }
    ```
    
    Logs:
    ```
    2024-07-03 15:27:42.342 [info] Using 'path' setting: /Users/dhruv/work/astral/ruff/target/debug/ruff
    2024-07-03 15:27:42.342 [info] Server run command: /Users/dhruv/work/astral/ruff/target/debug/ruff server --preview
    ```
    
    ### 4
    
    ```json
    {
      "ruff.nativeServer": true,
      "ruff.interpreter": [
        "/Users/dhruv/work/astral/ruff-lsp/.venv/bin/python"
      ]
    }
    ```
    
    Logs:
    ```
    2024-07-03 15:35:23.578 [info] Using the Ruff binary: /Users/dhruv/work/astral/ruff-lsp/.venv/bin/ruff
    2024-07-03 15:35:23.578 [info] Server run command: /Users/dhruv/work/astral/ruff-lsp/.venv/bin/ruff server --preview
    ```
    
    ### 5
    
    What if the script failed? I modified the script path in TypeScript code
    and we get the following logs:
    
    ```
    2024-07-03 15:42:41.035 [error] Error while trying to find the Ruff binary: Error: Command failed: /Users/dhruv/work/astral/ruff-lsp/.venv/bin/python /Users/dhruv/work/astral/ruff-vscode/bundled/tool/find_ruff_binary_pat.py
    /Users/dhruv/work/astral/ruff-lsp/.venv/bin/python: can't open file '/Users/dhruv/work/astral/ruff-vscode/bundled/tool/find_ruff_binary_pat.py': [Errno 2] No such file or directory
    
    2024-07-03 15:42:41.037 [info] Using environment executable: /Users/dhruv/.local/bin/ruff
    2024-07-03 15:42:41.040 [info] Server run command: /Users/dhruv/.local/bin/ruff server --preview
    ```

---
+++ bundled/tool/find_ruff_binary_path.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import sysconfig
+from pathlib import Path
+from typing import Optional
+
+RUFF_EXE = "ruff.exe" if sys.platform == "win32" else "ruff"
+
+
+def find_ruff_binary_path() -> Optional[Path]:
+    """Return the ruff binary path if it exists, `None` otherwise."""
+    bin_path = Path(sysconfig.get_path("scripts")) / RUFF_EXE
+    if bin_path.is_file():
+        return bin_path
+
+    if sys.version_info >= (3, 10):
+        user_scheme = sysconfig.get_preferred_scheme("user")
+    elif os.name == "nt":
+        user_scheme = "nt_user"
+    elif sys.platform == "darwin" and sys._framework:
+        user_scheme = "osx_framework_user"
+    else:
+        user_scheme = "posix_user"
+
+    scripts_path = Path(sysconfig.get_path("scripts", scheme=user_scheme)) / RUFF_EXE
+    if scripts_path.is_file():
+        return scripts_path
+
+    return None
+
+
+if __name__ == "__main__":
+    ruff_binary_path = find_ruff_binary_path()
+    if ruff_binary_path:
+        print(os.fsdecode(str(ruff_binary_path)), flush=True)
--- src/common/server.ts
+++ src/common/server.ts
@@ -1,8 +1,8 @@
 import * as fsapi from "fs-extra";
+import * as vscode from "vscode";
 import { Disposable, l10n, LanguageStatusSeverity, LogOutputChannel } from "vscode";
 import { State } from "vscode-languageclient";
 import {
-  Executable,
   LanguageClient,
   LanguageClientOptions,
   RevealOutputChannelOn,
@@ -14,7 +14,8 @@ import {
   RUFF_SERVER_REQUIRED_ARGS,
   RUFF_SERVER_SUBCOMMAND,
   RUFF_LSP_SERVER_SCRIPT_PATH,
-  NATIVE_SERVER_SCRIPT_PATH,
+  FIND_RUFF_BINARY_SCRIPT_PATH,
+  RUFF_BINARY_NAME,
 } from "./constants";
 import { traceError, traceInfo, traceVerbose } from "./log/logging";
 import { getDebuggerPath } from "./python";
@@ -27,23 +28,54 @@ import {
 import { updateStatus } from "./status";
 import { getProjectRoot } from "./utilities";
 import { isVirtualWorkspace } from "./vscodeapi";
+import { exec } from "child_process";
+import which = require("which");
 
-export type IInitOptions = {
+export type IInitializationOptions = {
   settings: ISettings[];
   globalSettings: ISettings;
 };
 
-function findNativeServerExecutable(settings: ISettings): Executable {
+/**
+ * Function to execute a command and return the stdout.
+ */
+function executeCommand(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, _) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+/**
+ * Finds the Ruff binary path and returns it.
+ *
+ * The strategy is as follows:
+ * 1. If the 'path' setting is set, check each path in order. The first valid
+ *    path is returned.
+ * 2. If the 'importStrategy' setting is 'useBundled', return the bundled
+ *    executable path.
+ * 3. Execute a Python script that tries to locate the binary. This uses either
+ *    the user-provided interpreter or the interpreter provided by the Python
+ *    extension.
+ * 4. If the Python script doesn't return a path, check the global environment
+ *    which checks the PATH environment variable.
+ * 5. If all else fails, return the bundled executable path.
+ */
+async function findRuffBinaryPath(
+  settings: ISettings,
+  outputChannel: LogOutputChannel,
+): Promise<string> {
   // 'path' setting takes priority over everything.
   if (settings.path.length > 0) {
     for (const path of settings.path) {
-      if (fsapi.existsSync(path)) {
+      if (await fsapi.pathExists(path)) {
         traceInfo(`Using 'path' setting: ${path}`);
-        return {
-          command: path,
-          args: [RUFF_SERVER_SUBCOMMAND, ...RUFF_SERVER_REQUIRED_ARGS],
-          options: { cwd: settings.cwd, env: process.env },
-        };
+        return path;
       }
     }
     traceInfo(`Could not find executable in 'path': ${settings.path.join(", ")}`);
@@ -51,20 +83,46 @@ function findNativeServerExecutable(settings: ISettings): Executable {
 
   if (settings.importStrategy === "useBundled") {
     traceInfo(`Using bundled executable: ${BUNDLED_RUFF_EXECUTABLE}`);
-    return {
-      command: BUNDLED_RUFF_EXECUTABLE,
-      args: [RUFF_SERVER_SUBCOMMAND, ...RUFF_SERVER_REQUIRED_ARGS],
-      options: { cwd: settings.cwd, env: process.env },
-    };
+    return BUNDLED_RUFF_EXECUTABLE;
   }
 
-  // Otherwise, we'll call a Python script that tries to locate a binary,
-  // falling back to the bundled version if no local executable is found.
-  return {
-    command: settings.interpreter[0],
-    args: [NATIVE_SERVER_SCRIPT_PATH, RUFF_SERVER_SUBCOMMAND, ...RUFF_SERVER_REQUIRED_ARGS],
-    options: { cwd: settings.cwd, env: process.env },
-  };
+  // Otherwise, we'll call a Python script that tries to locate a binary.
+  let ruffBinaryPath: string | undefined;
+  try {
+    const stdout = await executeCommand(
+      `${settings.interpreter[0]} ${FIND_RUFF_BINARY_SCRIPT_PATH}`,
+    );
+    ruffBinaryPath = stdout.trim();
+  } catch (err) {
+    await vscode.window
+      .showErrorMessage(
+        "Unexpected error while trying to find the Ruff binary. See the logs for more details.",
+        "Show Logs",
+      )
+      .then((selection) => {
+        if (selection) {
+          outputChannel.show();
+        }
+      });
+    traceError(`Error while trying to find the Ruff binary: ${err}`);
+  }
+
+  if (ruffBinaryPath && ruffBinaryPath.length > 0) {
+    // First choice: the executable found by the script.
+    traceInfo(`Using the Ruff binary: ${ruffBinaryPath}`);
+    return ruffBinaryPath;
+  }
+
+  // Second choice: the executable in the global environment.
+  const environmentPath = await which(RUFF_BINARY_NAME, { nothrow: true });
+  if (environmentPath) {
+    traceInfo(`Using environment executable: ${environmentPath}`);
+    return environmentPath;
+  }
+
+  // Third choice: bundled executable.
+  traceInfo(`Falling back to bundled executable: ${BUNDLED_RUFF_EXECUTABLE}`);
+  return BUNDLED_RUFF_EXECUTABLE;
 }
 
 async function createNativeServer(
@@ -72,14 +130,17 @@ async function createNativeServer(
   serverId: string,
   serverName: string,
   outputChannel: LogOutputChannel,
-  initializationOptions: IInitOptions,
+  initializationOptions: IInitializationOptions,
 ): Promise<LanguageClient> {
-  let serverOptions = findNativeServerExecutable(settings);
-  if (serverOptions.args) {
-    traceInfo(`Server run command: ${[serverOptions.command, ...serverOptions.args].join(" ")}`);
-  } else {
-    traceInfo(`Server run command: ${serverOptions.command}`);
-  }
+  const ruffBinaryPath = await findRuffBinaryPath(settings, outputChannel);
+  const ruffServerArgs = [RUFF_SERVER_SUBCOMMAND, ...RUFF_SERVER_REQUIRED_ARGS];
+  traceInfo(`Server run command: ${[ruffBinaryPath, ...ruffServerArgs].join(" ")}`);
+
+  let serverOptions = {
+    command: ruffBinaryPath,
+    args: ruffServerArgs,
+    options: { cwd: settings.cwd, env: process.env },
+  };
 
   const clientOptions = {
     // Register the server for python documents
@@ -105,7 +166,7 @@ async function createServer(
   serverId: string,
   serverName: string,
   outputChannel: LogOutputChannel,
-  initializationOptions: IInitOptions,
+  initializationOptions: IInitializationOptions,
 ): Promise<LanguageClient> {
   const command = settings.interpreter[0];
   const cwd = settings.cwd;
