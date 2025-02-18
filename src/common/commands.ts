40a600ff1f158bef9b568517a7e916fe1c9f735a
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-02-18 10:13:15 +0530

    Show document with server debug info

--- src/common/commands.ts
+++ src/common/commands.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
 import { ExecuteCommandRequest, LanguageClient } from "vscode-languageclient/node";
+import { getConfiguration } from "./vscodeapi";
+import { ISettings } from "./settings";
 
 const ISSUE_TRACKER = "https://github.com/astral-sh/ruff/issues";
 
@@ -35,4 +37,71 @@ async function executeCommand(lsClient: LanguageClient, command: string) {
       `Failed to execute the command '${command}'. Please consider opening an issue at ${ISSUE_TRACKER} with steps to reproduce.`,
     );
   });
+}
+
+export function createDebugInformationProvider(
+  getClient: () => LanguageClient | undefined,
+  serverId: string,
+  context: vscode.ExtensionContext,
+) {
+  let configuration = getConfiguration(serverId) as unknown as ISettings;
+  if (configuration.nativeServer === false || configuration.nativeServer === "off") {
+    return async () => {
+      vscode.window.showInformationMessage(
+        "Debug information is only available when using the native server",
+      );
+    };
+  }
+
+  const contentProvider = new (class implements vscode.TextDocumentContentProvider {
+    readonly uri = vscode.Uri.parse("ruff-server-debug://debug");
+    readonly eventEmitter = new vscode.EventEmitter<vscode.Uri>();
+
+    async provideTextDocumentContent(_uri: vscode.Uri): Promise<string> {
+      const lsClient = getClient();
+      if (!lsClient) {
+        return "";
+      }
+      const params = {
+        command: `${serverId}.printDebugInformation`,
+        arguments: [{ uri: vscode.window.activeTextEditor?.document.uri.toString() }],
+      };
+      return await lsClient.sendRequest(ExecuteCommandRequest.type, params).then(
+        (result) => {
+          if (typeof result === "string") {
+            return result;
+          }
+          return "";
+        },
+        async () => {
+          vscode.window.showErrorMessage(
+            `Failed to print debug information. Please consider opening an issue at ${ISSUE_TRACKER} with steps to reproduce.`,
+          );
+          return "";
+        },
+      );
+    }
+
+    get onDidChange(): vscode.Event<vscode.Uri> {
+      return this.eventEmitter.event;
+    }
+  })();
+
+  context.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider("ruff-server-debug", contentProvider),
+  );
+
+  return async () => {
+    contentProvider.eventEmitter.fire(contentProvider.uri);
+    const document = await vscode.workspace.openTextDocument(contentProvider.uri);
+    const content = document.getText();
+
+    // Show the document only if it has content.
+    if (content.length > 0) {
+      void (await vscode.window.showTextDocument(document, {
+        viewColumn: vscode.ViewColumn.Two,
+        preserveFocus: true,
+      }));
+    }
+  };
 }
