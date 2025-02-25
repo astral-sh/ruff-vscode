72c04ba75082d76dd739f2eb664d690116834d13
InSync <insyncwithfoo@gmail.com>
2025-02-25 17:53:00 +0700

    Make it easier for Windows users to test Red Knot (#703)
    ## Summary
    
    The extension now also recognizes `red_knot.exe` as a Red Knot affix.
    
    ## Test Plan
    
    None.
    
    ---------
    
    Co-authored-by: Dhruv Manilawala <dhruvmanila@gmail.com>

--- src/common/server.ts
+++ src/common/server.ts
@@ -175,7 +175,9 @@ async function createNativeServer(
 
   logger.info(`Found Ruff ${versionToString(ruffVersion)} at ${ruffBinaryPath}`);
 
-  if (!ruffBinaryPath.endsWith("red_knot")) {
+  const isRedKnot = ruffBinaryPath.endsWith("red_knot") || ruffBinaryPath.endsWith("red_knot.exe");
+
+  if (!isRedKnot) {
     if (!supportsNativeServer(ruffVersion)) {
       const message =
         `Native server requires Ruff ${versionToString(
@@ -192,7 +194,7 @@ async function createNativeServer(
   }
 
   let ruffServerArgs: string[];
-  if (ruffBinaryPath.endsWith("red_knot")) {
+  if (isRedKnot) {
     ruffServerArgs = [RUFF_SERVER_SUBCOMMAND];
   } else if (supportsStableNativeServer(ruffVersion)) {
     ruffServerArgs = [RUFF_SERVER_SUBCOMMAND];
